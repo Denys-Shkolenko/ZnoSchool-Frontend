756307705c7371ed0f4a2174e14ce8f05eb73d04
Denys-Shkolenko <denys.shkolenko@gmail.com>
2022-08-04 12:14:39 +0300

    disable transitions on window resize

--- js/burger-handler.js
+++ js/burger-handler.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.header-burger').click(function (event) {
+    $('.header-burger').click(function () {
         $('.header-burger,.menu-list,.authorization ').toggleClass('active');
     });
-}); 
+});
