97cceefd38fef1f8c7073132da31bc35223dda8c
Denys-Shkolenko <denys.shkolenko@gmail.com>
2022-08-17 17:41:38 +0300

    add: registration section

--- js/flip-card.js
+++ js/flip-card.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    $('.teachers .teachers-slider .item .rectangle').click(function () {
+    $('#teachers .teachers-slider .item .rectangle').click(function () {
         $('.teachers .teachers-slider .item .rectangle').toggleClass('active');
     });
 
-    $('.teachers .teachers-slider .slick-arrow').click(function () {
-        $('.teachers .teachers-slider .item .rectangle').removeClass('active');
+    $('#teachers .teachers-slider .slick-arrow').click(function () {
+        $('#teachers .teachers-slider .item .rectangle').removeClass('active');
     });
 });
