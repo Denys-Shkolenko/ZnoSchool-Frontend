daa10a13d3bd0f337130f7115d08951a002b7cc9
Denys-Shkolenko <denys.shkolenko@gmail.com>
2022-08-18 13:32:47 +0300

    add: contacts section

--- js/flip-card.js
+++ js/flip-card.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#teachers .teachers-slider .item .rectangle').click(function () {
-        $('.teachers .teachers-slider .item .rectangle').toggleClass('active');
+        $('#teachers .teachers-slider .item .rectangle').toggleClass('active');
     });
 
     $('#teachers .teachers-slider .slick-arrow').click(function () {
